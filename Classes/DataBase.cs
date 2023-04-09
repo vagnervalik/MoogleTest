8ed82a9908679145e8d676d19bde8d8053c87f4f
Vagner Valik <warpiano82@gmail.com>
2023-04-09 01:40:16 -0400

    Words Split Succesfully

--- Classes/DataBase.cs
+++ Classes/DataBase.cs
@@ -2,11 +2,15 @@ namespace Classes;
 
 class DataBase{
     private string address;
-    private string[] Docs;
+    private string[] AllText;
+    private string[][] Docs;
+
     public DataBase(string address){
         this.address = address;
-        this.Docs = this.LoadDataBase();
+        this.AllText = this.LoadDataBase();
+        this.Docs = this.GetAllWords();
     } 
+
     public string[] LoadDataBase(){
         string[] Files = Directory.GetFiles(address);
         string[] AllFiles = new String[Files.Length];
@@ -15,10 +19,29 @@ class DataBase{
         }
         return AllFiles;
     }
-    public string Get(int pos){
+
+    public string[][] GetAllWords(){
+        char[] chars = new char[]{' ', ',', '.', '`', '!', '~', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '-', '+', '=', '{', '}', '[', ']', ':', ';', '"', '\'', '\\', '|', '<', '>', '?', '/', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '\r' , '\n'};
+        List<string[]> AllWords = new List<string[]>();
+        for (int i = 0; i < this.Count(); i++){
+            AllWords.Add(this.AllText[i].Trim(chars).ToLower().Split(chars, StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries));
+        }
+        string[][] Words = AllWords.ToArray();
+        return Words;
+    }
+
+    public void Display(int pos){
+        int i = 1;
+        foreach(string word in this.Get(pos)){
+            Console.Write(i + " = " + $"'{word}'" + "   ");
+            i++;
+        }
+    }
+    public string[] Get(int pos){
         return this.Docs[pos];
     }
+
     public int Count(){
-        return this.Docs.Length;
+        return this.AllText.Length;
     }
 }
--- Program.cs
+++ Program.cs
@@ -7,5 +7,6 @@ class Program{
         DataBase Docs = new DataBase("Content");
         Console.WriteLine("Database loaded!");
         Console.WriteLine(Docs.Count());
+        Docs.Display(4);
     }
 }
